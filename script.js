d68088a3f2ab1a8f3424a8707ef39a05c94738d9
tomohiko-hasegawa <79678350+tomohiko-hasegawa@users.noreply.github.com>
2021-11-07 22:37:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $(".menu-nav").hide();
+  $(".meue-btn").click(function() {
+    $(".menu-nav").toggle(300);
+  });
+  //$("body").click(function() {
+  //  $(".menu-nav").hide(300);
+  //});
+});
