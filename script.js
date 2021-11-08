99fffc2f1b62b45005800db07d9a25c2318f685d
tomohiko-hasegawa <79678350+tomohiko-hasegawa@users.noreply.github.com>
2021-11-08 23:04:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ $(function() {
   $(".meue-btn").click(function() {
     $(".menu-nav").toggle(300);
   });
-  //$("body").click(function() {
-  //  $(".menu-nav").hide(300);
-  //});
+
+  $(".menu-nav a").click(function() {
+    $(".menu-nav").hide(300);
+  });
+
+  $('.scroll a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position
+    }, 500);
+    });
+
 });
